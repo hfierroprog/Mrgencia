de5abc9c62e40ed9fdc8e320c235c2398d4cb71d
hfierroprog <hfierroprog@gmail.com>
2017-10-10 07:24:43 -0300

    Cambios..

--- app/src/main/java/com/example/hbcapps/mrgencia/MainActivity.java
+++ app/src/main/java/com/example/hbcapps/mrgencia/MainActivity.java
@@ -27,32 +27,35 @@ public class MainActivity extends AppCompatActivity {
         llamar = (Button) findViewById(R.id.btnAyuda);
         final String numero = "+56958889732";
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, codigo_llamada);
+        } else {
+            OlderVersions(numero);
+        }
+
         llamar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            }
 
-            private void OlderVersions(String numero) {
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + numero));
-                if (checkPermission(Manifest.permission.CALL_PHONE)) {
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(MainActivity.this, "No has aceptado los permisos necesarios para la aplicación", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            private void NewerVersions() {
-                requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, codigo_llamada);
             }
         });
     }
 
+    private void OlderVersions(String numero) {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + numero));
+        if (checkPermission(Manifest.permission.CALL_PHONE)) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(MainActivity.this, "No has aceptado los permisos necesarios para la aplicación", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Metodo para manejar la respuesta despues de solicitar permisos
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        //Estamos en el caso del telefono
+        //Se diferencian los permisos mediante codigos, en este caso el de telefono es 100
         switch (requestCode) {
             case codigo_llamada:
-
                 String permission = permissions[0];
                 int result = grantResults[0];
 
@@ -76,10 +79,9 @@ public class MainActivity extends AppCompatActivity {
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
                 break;
         }
-
-
     }
 
+    //Comprueba si el usuario ha aceptado el permiso que se le pasa por parametro
     private boolean checkPermission(String permission){
         int result = this.checkCallingOrSelfPermission(permission);
         return result == PackageManager.PERMISSION_GRANTED;
